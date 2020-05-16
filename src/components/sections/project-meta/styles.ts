025b925fe9a0ca242e366e2fdcd97586e6418e52
Pat Jacobs <musicbender@gmail.com>
2020-05-15 23:40:00 -0700

    add icons

--- src/components/sections/project-meta/styles.ts
+++ src/components/sections/project-meta/styles.ts
@@ -1,21 +1,26 @@
 import styled, { css } from 'styled-components';
+import { getRandomColor } from '../../../util/colors';
 import { CoreColors } from '../../../../types';
 
 interface ContentProps {
     color?: CoreColors;
 }
 
+const conf = {
+    desktopMargin: '3em',
+};
+
 export const ProjectMetaWrapper = styled.div`
-    width: ${({ theme }) => `${theme.gridSizes.m}%`};
+    overflow: hidden;
 `;
 
 export const ContentWrapper = styled.div`
     width: 100%;
-    margin: 3em auto;
+    margin: ${`${conf.desktopMargin} auto`};
 `;
 
 const content = css`
-    margin: 0.55em 0;
+    margin: 0.65em 0;
     font-weight: 200;
     font-size: 0.9rem;
     letter-spacing: 4px;
@@ -42,4 +47,33 @@ export const ContentValueLink = styled('a')<ContentProps>`
     }
 `;
 
-export const IconsWrapper = styled.div``;
+export const IconsWrapper = styled.div`
+    width: 100%;
+    max-width: 12em;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr;
+    margin: ${`${conf.desktopMargin} 0`};
+`;
+
+export const IconWrapper = styled.a`
+    justify-self: center;
+    &:first-child {
+        justify-self: left;
+    }
+    &:nth-child(3) {
+        justify-self: right;
+    }
+    &:focus,
+    &:hover {
+        > svg {
+            fill: ${getRandomColor()};
+        }
+    }
+`;
+
+export const Icon = styled.svg`
+    width: 2em;
+    fill: ${({ theme }) => theme.palette.white};
+    transition: ${({ theme }) => `fill ${theme.animate.fast}`};
+`;
