7a78783028651ab39b27346192eda6be58895667
Pat Jacobs <musicbender@gmail.com>
2022-05-12 17:01:37 -0700

    head changes and manifest definitions for pwa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,11 +10,12 @@ import Document, {
   Head,
   NextScript,
 } from 'next/document';
+import theme from '@styles/theme';
 export default class MyDocument extends Document {
   seo: Seo = {
     title: meta.title,
     description: meta.description,
-    image: `${meta.siteUrl}/assets/thumbnail.png`,
+    image: `${meta.siteUrl}/assets/screenshot-1.png`,
     twitter: meta.twitter,
     site: meta.site,
     color: meta.color,
@@ -57,14 +58,23 @@ export default class MyDocument extends Document {
           <meta property="og:title" content={this.seo.title} />
           <meta property="og:description" content={this.seo.description} />
           <meta property="og:image" content={this.seo.image} />
+          <meta property="og:image:url" content={this.seo.image} />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-title" content={this.seo.site} />
           <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
           <meta name="twitter:creator" content={this.seo.twitter} />
+          <meta name="twitter:site" content={this.seo.twitter} />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:title" content={this.seo.title} />
           <meta name="twitter:description" content={this.seo.description} />
           <meta name="twitter:image" content={this.seo.image} />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=4" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=4" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=4" />
+          <link rel="manifest" href="/site.webmanifest?v=4" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg?v=4" color={theme.corePalette.aqua} />
+          <link rel="shortcut icon" href="/favicon.ico?v=4" />
+          <meta name="msapplication-TileColor" content={this.seo.color} />
         </Head>
         <body>
           <Main />
